1dca7bc0e3ed4baad42756e657ef36696a128997
kuuhaku <nguyenmanhdzung5920@gmail.com>
2023-05-26 14:51:11 +0700

    update Download CV

--- mycv/hello_world/urls.py
+++ mycv/hello_world/urls.py
@@ -4,4 +4,5 @@ from hello_world import views
 urlpatterns = [
     path('', views.cv, name='cv'),
     path('cv1', views.cv1, name='cv1'),
+    path('download/', views.download_file, name='download_file'),
 ]
--- mycv/hello_world/views.py
+++ mycv/hello_world/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import os
+import mimetypes
+from django.http.response import HttpResponse
 
 # Create your views here.
 def cv(request):
@@ -6,3 +9,21 @@ def cv(request):
 
 def cv1(request):
     return render(request, "cv1.html", {})
+
+def download_file(request):
+    # Define Django project base directory
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # Define text file name
+    filename = "Resume_AI-Engineer_NguyenManhDung.pdf"
+    # Define the full file path
+    filepath = BASE_DIR + "/hello_world/Files/" + filename
+    # Open the file for reading content
+    path = open(filepath, 'rb')
+    # Set the mime type
+    mime_type, _ = mimetypes.guess_type(filepath)
+    # Set the return value of the HttpResponse
+    response = HttpResponse(path, content_type=mime_type)
+    # Set the HTTP header for sending to browser
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    # Return the response value
+    return response
